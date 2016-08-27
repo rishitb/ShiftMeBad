4b4e1a089b13f8b964ba5f10ece717d8e583432e
atitk@andrew.cmu.edu <kothari.atit@gmail.com>
2016-08-27 14:44:11 -0400

    Player Controller now obeys 2D Physics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,11 +3,13 @@ using UnityEngine.EventSystems;
 using System.Collections;
 using System;
 
-public class PlayerController : MonoBehaviour,IPointerDownHandler, IDragHandler {
+public class PlayerController : MonoBehaviour,IPointerDownHandler, IDragHandler,IPointerUpHandler {
 
     [Tooltip("Smoothness in movement while dragging")]
     public float smoothness;
 
+    public Rigidbody2D grabbedObject;
+    Vector2 originalPos=Vector2.zero;
 	// Use this for initialization
 	void Start () {
 	
@@ -15,22 +17,56 @@ public class PlayerController : MonoBehaviour,IPointerDownHandler, IDragHandler 
 	
 	// Update is called once per frame
 	void Update () {
-	    
+      
 	}
+
+    void FixedUpdate()
+    {
+        if (grabbedObject!=null)
+        {
+          
+            Vector3 MouseWorldPos3D =  Input.mousePosition;
+            Vector2 MouseScreenPos2D = new Vector2(MouseWorldPos3D.x, MouseWorldPos3D.y);
+
+            Vector2 objScreenPostion = Camera.main.WorldToScreenPoint(grabbedObject.position);
+
+            Vector2 dir = MouseScreenPos2D - objScreenPostion;
+
+            dir *= smoothness;
+
+            grabbedObject.velocity = dir;
+            
+        }
+    }
 
     public void SelectObject()
     {
         Debug.Log("Selecting object:" + gameObject.name);
         ShapeGlobalVars.selectedObject = this.gameObject;        
     }
+    
 
     public void OnDrag(PointerEventData eventData)
     {
-        transform.position = Vector2.Lerp(transform.position,Input.mousePosition,smoothness);
+   
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         SelectObject();
+        grabbedObject = ShapeGlobalVars.selectedObject.GetComponent<Rigidbody2D>();
+
+        if (gameObject.GetComponent<ShapeShifter>().currentShape.shapeName == ShapeGlobalVars.ShapeStyle.Circle)
+            gameObject.GetComponent<Rigidbody2D>().freezeRotation = true;
+
+        originalPos=grabbedObject.position;
     }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        grabbedObject.velocity = Vector2.zero;
+        grabbedObject = null;
+    }
+
+   
 }
